f373cfe5c659e6c4327f8f7f8f8be24255b23eda
Pratik <punchaal@gmail.com>
2019-08-07 11:23:21 -0400

    Making changes

---
+++ build/precache-manifest.74d6b55fdb564e189557932d3e55db2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb35c380f70dac1812e4e617f286525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19798900fe3fba5f426",
+    "url": "/static/js/2.11ab1160.chunk.js"
+  },
+  {
+    "revision": "a4250ad7e9183f2359d2",
+    "url": "/static/js/main.7b386ab2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d01060d0fda5e80a56e0aca33f3495d",
+    "url": "/static/media/home-screen.2d01060d.jpg"
+  }
+]);
--- tuneful-server/src/Test.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const app = express()
-
-
-app.use(cookieParser());
-
-console.log(process.env.SPOTIFY_CLIENT_ID)
